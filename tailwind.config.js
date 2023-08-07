c5675462c60a4e169c1f904370da6e333c0d6188
Krjorn <christopher990172@gmail.com>
2023-08-07 18:25:59 -0300

    Extended themes for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     extend: {
       fontFamily: {
         'inter': ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'background': '#FFFFFF',
+        'primary': '#3A404E',
+        'secondary': '#8E2424',
+        'tertiary': '#667085',
+      },
+      padding: {
+        '18': '1.125rem',
       }
     },
   },
