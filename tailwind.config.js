373b5c2ed11cc7d6fa45d8b10a4fc111a12d25c0
Krjorn <christopher990172@gmail.com>
2023-08-07 17:13:12 -0300

    Added font Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./site/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
