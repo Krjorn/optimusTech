7627bb45fb3ff067248ddc65733c4abe5d2af183
Krjorn <christopher990172@gmail.com>
2023-08-09 14:37:28 -0300

    Made metrics section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,19 @@ module.exports = {
         'background': '#FFFFFF',
         'primary': '#3A404E',
         'secondary': '#8E2424',
+        'secondary-o-1': 'rgba(142, 36, 36, 0.05)',
+        'secondary-0-2': 'rgba(195, 25, 25, 0.03)',
+        'g-0': '#F9FAFB',
         'g-1': '#98A2B3',
         'g-2': '#667085',
         'g-3': '#101828',
+        'g-border': '#EAECF0',
       },
       padding: {
         '18': '1.125rem',
+      },
+      maxWidth: {
+        '280': '280px',
       }
     },
   },
