dc3fb0d3453dc609eb2c1992063d080d2547c12e
Krjorn <christopher990172@gmail.com>
2023-08-10 14:50:02 -0300

    Made jobs - 1st part

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'primary': '#3A404E',
         'secondary': '#8E2424',
         'secondary-o-1': 'rgba(142, 36, 36, 0.05)',
-        'secondary-0-2': 'rgba(195, 25, 25, 0.03)',
+        'secondary-o-2': 'rgba(195, 25, 25, 0.03)',
         'g-0': '#F9FAFB',
         'g-1': '#98A2B3',
         'g-2': '#667085',
